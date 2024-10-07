b982eca5fa810c3f5d6d21c32bdd3191d2756578
AnkithaVRamaiah <113353061+AnkithaVRamaiah@users.noreply.github.com>
2024-10-07 12:20:16 +0530

    Update aws-resource-usage.sh

--- Project2/aws-resource-usage.sh
+++ Project2/aws-resource-usage.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-#author: ankitha
-#date: 12/05/2024
-#version:v1
-#this script will report the aws resource usage
+# author: Ankita
+# date: 12/05/2024
+# version: v1
+# this script will report the AWS resource usage
 
-#aws s3
-#aws ec2
-#aws lambda
-#aws iam users
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM users
 
+# Cron Job Setup:
+# To run this script every day at 6 AM, add the following line to your crontab:
+# 0 6 * * * /path/to/your/script.sh
+# This ensures that the script is executed daily without manual intervention.
 
 set -x
 
-#list of s3 bucketss
-echo "print list of s3 buckets"
+# List of S3 buckets
+echo "Print list of S3 buckets"
 aws s3 ls
 
-#list of ec2 instances
-echo "print list of ec2 instances"
+# List of EC2 instances
+echo "Print list of EC2 instances"
 aws ec2 describe-instances
 
-#list of lambda functions
-echo "print list of lambda functions"
+# List of Lambda functions
+echo "Print list of Lambda functions"
 aws lambda list-functions
 
-#list of IAM users
-echo "print list of IAM users"
+# List of IAM users
+echo "Print list of IAM users"
 aws iam list-users
